c2097f10d60e46a35c3101708be05bd18e587a10
RobiatAbdulazeez <122813799+RobiatAbdulazeez@users.noreply.github.com>
2023-11-06 00:23:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
 $(function () {
-    $('#photo-viewer').customPhotoViewer().show().on('click', '.photo-box', function (e) {
+    $('#photo-viewer').show().on('click', '.photo-box', function x(e) {
         var $content = $(this).clone().find('img').css({
             marginLeft: 0,
             marginTop: 0,
             width: '100%',
             height: 'auto'
         });
-        //modal code goes here
+        // Get the modal
+        var modal = $("#myModal");
+
+
+
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var modalImg = $("#img");
+        $('.photo-box').on('click', function (e) {
+            $(modal).show()
+            $(modalImg).attr('src', $('.active').children('.myImg').map(function () {
+                return $(this).attr('src')
+            }).get());
+        })
+
+        // Get the <span> element that closes the modal
+        var span = $(".close");
+
+        // When the user clicks on <span> (x), close the modal
+        span.on('click', function (e) {
+            e.preventDefault();
+            $(modal).hide();
+        })
+
+        $('#close').on('click', function () {
+
+            $(".modal-content").hide()
+        });
     });
 });
+
