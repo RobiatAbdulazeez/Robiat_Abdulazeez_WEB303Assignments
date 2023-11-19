092e2684a8accc513e0ab489cd01eb1fa3d84eff
RobiatAbdulazeez <122813799+RobiatAbdulazeez@users.noreply.github.com>
2023-11-19 16:22:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,69 +1,116 @@
-let charactersData; // Store the original characters data
-
 $(document).ready(function () {
-  $.ajax({
-    type: "get",
-    url: "exm.json",
-    dataType: 'json',
-    success: function (data) {
-      charactersData = data; // Store the original characters data
-      displayCharacters(data);
+    let $table = $('<table/>');
+    $('body').append($table);
 
-      $('#search').on('input', function () {
-        highlightRows($(this).val());
-      });
+    $('table').after('<div id="filterButtonsContainer"/>');
+    $('#filterButtonsContainer').append('<button id="filterAM">A - M (0)</button>');
+    $('#filterButtonsContainer').append('<button id="filterNZ">N - Z (0)</button>');
+    $('h1').after('<input/>');
+    $('input').attr('id', 'searchInput'); // Corrected the input id
 
-      $('#filterAM').on('click', function () {
-        filterCharacters('A', 'M');
-      });
+    // add thead and tbody to the table
+    $('table').append('<thead/>');
+    $('table').append('<tbody/>');
+    $('table').addClass('tb1');
 
-      $('#filterNZ').on('click', function () {
-        filterCharacters('N', 'Z');
-      });
-    },
-    error: function () {
-      alert('Failed to retrieve data.');
+    // create the header row
+    let $headerRow = $('<tr/>').addClass('headerRow');
+    // add cells to the header row
+    $headerRow.append($('<td/>').text('firstName'));
+    $headerRow.append($('<td/>').text('lastName'));
+    $headerRow.append($('<td/>').text('gender'));
+    $headerRow.append($('<td/>').text('role'));
+    $headerRow.append($('<td/>').text('age'));
+
+    // add header row to thead
+    $('thead').append($headerRow);
+
+    // Update the success function for the AJAX request
+    $.ajax({
+        type: 'get',
+        url: 'exm.json',
+        error: function () {
+            $('.tb1').empty().append('<h1> File not found </h1>');
+        },
+        success: function (response) {
+            // Your existing code...
+            $.each(response, function (index, value) {
+                // create row
+                let $row = $('<tr/>').addClass('row');
+                // add td to the row
+                $row.append($('<td></td>').text(value.firstName));
+                $row.append($('<td></td>').text(value.lastName)); // Corrected the property name
+                $row.append($('<td></td>').text(value.gender));
+                $row.append($('<td id="role"></td>').text(value.role)); // Corrected the property name
+                $row.append($('<td></td>').text(value.age)); // Corrected the property name
+
+                // add row to the table
+                $('table').append($row);
+                let $gender = $('tbody #gender');
+
+            });
+
+            // Add event listener for the search input
+            $('#searchInput').on('input', function () {
+                searchCharacters($(this).val());
+            });
+
+            // Add event listeners for filter buttons
+            $('#filterAM').on('click', function () {
+                filterCharacters('A', 'M');
+            });
+
+            $('#filterNZ').on('click', function () {
+                filterCharacters('N', 'Z');
+            });
+        }
+    });
+
+    // Add the searchCharacters function
+    function searchCharacters(searchTerm) {
+        $('tbody tr').each(function () {
+            let firstName = $(this).find('td:first').text();
+            let containsTerm = firstName.toLowerCase().includes(searchTerm.toLowerCase());
+
+            if (containsTerm) {
+                $(this).addClass('highlighted');
+            } else {
+                $(this).removeClass('highlighted');
+            }
+        });
     }
-  });
+
+    // Add the filterCharacters function
+    function filterCharacters(startRange, endRange) {
+        var tableRows = $('tbody tr');
+
+        tableRows.each(function () {
+            var lastName = $(this).find('td:nth-child(2)').text();
+            var firstLetter = lastName.charAt(0).toUpperCase();
+            var isInRange = firstLetter >= startRange && firstLetter <= endRange;
+
+            $(this).toggle(isInRange);
+        });
+
+        updateFilterButtonCount();
+    }
+
+    // Add the updateFilterButtonCount function
+    function updateFilterButtonCount() {
+        var countAM = $('tbody tr:visible').filter(function () {
+            var lastName = $(this).find('td:nth-child(2)').text();
+            var firstLetter = lastName.charAt(0).toUpperCase();
+            return firstLetter >= 'A' && firstLetter <= 'M';
+        }).length;
+
+        var countNZ = $('tbody tr:visible').filter(function () {
+            var lastName = $(this).find('td:nth-child(2)').text();
+            var firstLetter = lastName.charAt(0).toUpperCase();
+            return firstLetter >= 'N' && firstLetter <= 'Z';
+        }).length;
+
+        $('#filterAM').text('A - M (' + countAM + ')');
+        $('#filterNZ').text('N - Z (' + countNZ + ')');
+    }
 });
 
-function displayCharacters(characters) {
-  $('#charactersTable tbody').empty();
-  $.each(characters, function (index, character) {
-    $('#charactersTable tbody').append(`
-                <tr>
-                    <td>${character.firstName}</td>
-                    <td>${character.lastName}</td>
-                    <td>${character.gender}</td>
-                    <td>${character.role}</td>
-                    <td>${character.age}</td>
-                </tr>
-            `);
-  });
-  // Reset highlighting when displaying characters
-  $('#charactersTable tbody tr').removeClass('highlight');
-}
-
-function highlightRows(searchTerm) {
-  searchTerm = searchTerm.toLowerCase().trim(); // Trim leading and trailing whitespaces
-  
-  $('#charactersTable tbody tr').each(function () {
-    const firstName = $(this).find('td:first').text().toLowerCase();
-    
-    // Check if the search term is not empty and is included in the first name
-    if (searchTerm !== '' && firstName.includes(searchTerm)) {
-      $(this).addClass('highlight');
-    } else {
-      $(this).removeClass('highlight');
-    }
-  });
-}
-
-function filterCharacters(startLetterA, startLetterB) {
-  const filteredData = charactersData.filter(function (character) {
-    const lastName = character.lastName.charAt(0).toUpperCase();
-    return lastName >= startLetterA && lastName <= startLetterB;
-  });
-
-  displayCharacters(filteredData);
-}
