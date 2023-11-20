0f9e522dea6272d3a68511aacc3c2bbfc17b57f7
RobiatAbdulazeez <122813799+RobiatAbdulazeez@users.noreply.github.com>
2023-11-19 20:48:09 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,116 +1,127 @@
-$(document).ready(function () {
-    let $table = $('<table/>');
-    $('body').append($table);
+// add a table to the page 
+let $table = $('<table/>');
+// add to the body of document
+$('body').append($table);
 
-    $('table').after('<div id="filterButtonsContainer"/>');
-    $('#filterButtonsContainer').append('<button id="filterAM">A - M (0)</button>');
-    $('#filterButtonsContainer').append('<button id="filterNZ">N - Z (0)</button>');
-    $('h1').after('<input/>');
-    $('input').attr('id', 'searchInput'); // Corrected the input id
+// add a title for the table before table 
 
-    // add thead and tbody to the table
-    $('table').append('<thead/>');
-    $('table').append('<tbody/>');
-    $('table').addClass('tb1');
+$('table').before('<h1/>');
+$('h1').text('Sorting Characters');
 
-    // create the header row
-    let $headerRow = $('<tr/>').addClass('headerRow');
-    // add cells to the header row
-    $headerRow.append($('<td/>').text('firstName'));
-    $headerRow.append($('<td/>').text('lastName'));
-    $headerRow.append($('<td/>').text('gender'));
-    $headerRow.append($('<td/>').text('role'));
-    $headerRow.append($('<td/>').text('age'));
+// add a thead and tbody on the table 
+$('table').append('<thead/>');
+$('table').append('<tbody/>');
+$('table').addClass('sortable');
 
-    // add header row to thead
-    $('thead').append($headerRow);
+//create the heading row
+let $headingRow = $('<tr/>');
+// add this row to thead
+$('thead').append($headingRow);
+$headingRow.append($('<th/>').html('<a data-sort="name">First Name</a>'));
+$headingRow.append($('<th/>').html('<a data-sort="name">Last Name</a>'));
+$headingRow.append($('<th/>').html('<a data-sort="name">Gender</a>'));
+$headingRow.append($('<th/>').html('<a data-sort="name">Role</a>'));
+$headingRow.append($('<th/>').html('<a data-sort="name">Age</a>'));
+$headingRow.append($('<th/>').html('<a data-sort="date">Date Of Birth</a>'));
 
-    // Update the success function for the AJAX request
-    $.ajax({
-        type: 'get',
-        url: 'exm.json',
-        error: function () {
-            $('.tb1').empty().append('<h1> File not found </h1>');
-        },
-        success: function (response) {
-            // Your existing code...
-            $.each(response, function (index, value) {
-                // create row
-                let $row = $('<tr/>').addClass('row');
-                // add td to the row
-                $row.append($('<td></td>').text(value.firstName));
-                $row.append($('<td></td>').text(value.lastName)); // Corrected the property name
-                $row.append($('<td></td>').text(value.gender));
-                $row.append($('<td id="role"></td>').text(value.role)); // Corrected the property name
-                $row.append($('<td></td>').text(value.age)); // Corrected the property name
+// get content of json file 
 
-                // add row to the table
-                $('table').append($row);
-                let $gender = $('tbody #gender');
+$.ajax({
+    type: "get",
+    url: "exm.json",
+    error: function(){
+        $('.sortable').empty().append('<h1> Content can not be retrieved</h1>');
+    },
+    success: function(response){
+        //loop through response received
+        $.each(response, function(index, value){
+            // create row
+            let $row = $('<tr/>').addClass('row');
+            // add td to the row
+            $row.append($('<td></td>').text(value.firstName));
+            $row.append($('<td></td>').text(value.lastName));
+            $row.append($('<td></td>').text(value.gender));
+            $row.append($('<td></td>').text(value.role));
+            $row.append($('<td></td>').text(value.age));
+            $row.append($('<td></td>').text(value.date-of-birth));
 
-            });
-
-            // Add event listener for the search input
-            $('#searchInput').on('input', function () {
-                searchCharacters($(this).val());
-            });
-
-            // Add event listeners for filter buttons
-            $('#filterAM').on('click', function () {
-                filterCharacters('A', 'M');
-            });
-
-            $('#filterNZ').on('click', function () {
-                filterCharacters('N', 'Z');
-            });
-        }
-    });
-
-    // Add the searchCharacters function
-    function searchCharacters(searchTerm) {
-        $('tbody tr').each(function () {
-            let firstName = $(this).find('td:first').text();
-            let containsTerm = firstName.toLowerCase().includes(searchTerm.toLowerCase());
-
-            if (containsTerm) {
-                $(this).addClass('highlighted');
-            } else {
-                $(this).removeClass('highlighted');
+            // add rows to table 
+            $('tbody').append($row);
+        
+        //Start Sorting
+        var compare = {
+            name: function(a,b){
+                a = a.replace(/^the /i, '');
+                b =  b.replace(/^the /i, '');
+        
+                if (a < b){
+                    return -1;
+                } else {
+                    return a>b ? 1 : 0;
+                }
+            },
+            duration: function(a,b){
+                a = a.split('-');
+                b = b.split('-');
+        
+                return a - b;
+        
+            },
+            date: function(a,b){
+                a = new Date(a);
+                b = new Date(b);
+        
+                return a - b;
             }
-        });
+        };
+        
+        $('.sortable').each(function(){
+            var $table = $(this);
+            var $tbody = $table.find('tbody');
+            var $controls = $table.find('th a');
+            var rows = $tbody.find('tr').toArray();
+            //copy/clown the row that is existing row
+            const deepCopy =[...rows];
+        
+            $controls.on('click',function(){
+                var $header = $(this);
+                var order = $header.data('sort');
+                var column;
+              
+                //If selected item has ascending or descending class, reverse contents
+                if ($header.is('.ascending')){
+                    $header.removeClass(".ascending no-sort");
+                    $headingRow.addClass(".descending");
+                   
+                    $tbody.append(rows.reverse());
+                    
+        
+                }else if($header.is('.decending')){
+                    $header.removeClass('descending ascending ');
+                    $header.addClass('no-sort');
+                    $tbody.append(deepCopy);
+
+                } else {
+                    $header.addClass('ascending');
+                    $header.removeClass('no-sort');
+                    //Remove asc or desc from all other headers
+                    $header.siblings().removeClass('ascending descending no-sort');
+                    
+                    if (compare.hasOwnProperty(order)){
+                        column = $controls.index(this);
+                     rows.sort(function(a,b){
+                            a = $(a).find('td').eq(column).text();
+                            b = $(b).find('td').eq(column).text();
+                            console.log('a: ',a,'   b: ', b)
+                            return  compare[order](a,b);
+                            
+                            
+                        });
+                        $tbody.append(rows);
+                    }
+                }
+            })
+        })
+    });
     }
-
-    // Add the filterCharacters function
-    function filterCharacters(startRange, endRange) {
-        var tableRows = $('tbody tr');
-
-        tableRows.each(function () {
-            var lastName = $(this).find('td:nth-child(2)').text();
-            var firstLetter = lastName.charAt(0).toUpperCase();
-            var isInRange = firstLetter >= startRange && firstLetter <= endRange;
-
-            $(this).toggle(isInRange);
-        });
-
-        updateFilterButtonCount();
-    }
-
-    // Add the updateFilterButtonCount function
-    function updateFilterButtonCount() {
-        var countAM = $('tbody tr:visible').filter(function () {
-            var lastName = $(this).find('td:nth-child(2)').text();
-            var firstLetter = lastName.charAt(0).toUpperCase();
-            return firstLetter >= 'A' && firstLetter <= 'M';
-        }).length;
-
-        var countNZ = $('tbody tr:visible').filter(function () {
-            var lastName = $(this).find('td:nth-child(2)').text();
-            var firstLetter = lastName.charAt(0).toUpperCase();
-            return firstLetter >= 'N' && firstLetter <= 'Z';
-        }).length;
-
-        $('#filterAM').text('A - M (' + countAM + ')');
-        $('#filterNZ').text('N - Z (' + countNZ + ')');
-    }
-});
-
+})
